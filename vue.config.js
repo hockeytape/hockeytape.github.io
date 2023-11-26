60bae7d66280fac881666e5f4bface07ed5a1710
hockeytape <hockeytape@protonmail.com>
2023-11-26 00:34:55 -0800

    remove all files

--- vue.config.js
+++
@@ -1,7 +0,0 @@
-// make a module.exports file here that puts the path
-
-module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-        ? '/hockeytape.github.io/'
-        : '/'
-};
